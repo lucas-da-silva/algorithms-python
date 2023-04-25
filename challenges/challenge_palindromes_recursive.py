8e186611354fb47ea787f90ceae0067415420f2a
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-25 16:30:40 -0300

    feat: create is_palindrome_recursive function to return if a string is palindrome

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(
+    word: str, low_index: int, high_index: int
+) -> bool:
+    try:
+        if word[low_index] != word[high_index]:
+            return False
+
+        if low_index >= high_index:
+            return True
+
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    except IndexError:
+        return False
