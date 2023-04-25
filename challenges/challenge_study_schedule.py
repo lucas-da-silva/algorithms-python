91eea1bc51c4fad1c56b90ed08c69adc4c7218e9
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-25 15:03:59 -0300

    refactor: using sum function

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,10 @@
 def study_schedule(permanence_period: list[tuple], target_time: int) -> int:
-    best_period = 0
-
     try:
-        for entry, leave in permanence_period:
-            if entry <= target_time <= leave:
-                best_period += 1
+        studying_at_the_same_time = sum(
+            1
+            for entry, leave in permanence_period
+            if entry <= target_time <= leave
+        )
     except TypeError:
         return None
-
-    return best_period
+    return studying_at_the_same_time
