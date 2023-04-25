96d227766bf91113b928851a28d43353b146ac3b
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-25 14:53:20 -0300

    feat: create study_schedule function to return student number in a timetable

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period: list[tuple], target_time: int) -> int:
+    best_period = 0
+
+    try:
+        for entry, leave in permanence_period:
+            if entry <= target_time <= leave:
+                best_period += 1
+    except TypeError:
+        return None
+
+    return best_period
