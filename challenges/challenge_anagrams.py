0bfc7145af6518bf609a7fd4aae172830dfbd703
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-25 18:39:29 -0300

    feat: create is_anagram function to return if two strings are anagrams of each other

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,45 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def merge_sort(string: str) -> str:
+    if len(string) <= 1:
+        return string
+
+    mid = len(string) // 2
+    left = string[:mid]
+    right = string[mid:]
+
+    ordered_left = merge_sort(left)
+    ordered_right = merge_sort(right)
+
+    return "".join(merge(ordered_left, ordered_right))
+
+
+def merge(left: list[str], right: list[str]) -> list[str]:
+    result = []
+    i, j = 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    result += left[i:]
+    result += right[j:]
+
+    return result
+
+
+def is_anagram(first_string: str, second_string: str) -> tuple:
+    first_string_lower = first_string.lower() if first_string else ""
+    second_string_lower = second_string.lower() if second_string else ""
+
+    ordered_first_string = merge_sort(first_string_lower)
+    ordered_second_string = merge_sort(second_string_lower)
+
+    if not first_string or not second_string:
+        return (ordered_first_string, ordered_second_string, False)
+
+    are_anagrams = ordered_first_string == ordered_second_string
+
+    return (ordered_first_string, ordered_second_string, are_anagrams)
