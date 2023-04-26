8ad089828cd741453399b89db9d31696fe8324bb
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-26 10:02:51 -0300

    feat: implements validate_nums function to return if have duplicate number in a list

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,22 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def validate_nums(nums: list[int]) -> bool:
+    try:
+        if any(n < 0 for n in nums):
+            return False
+    except TypeError:
+        return False
+
+    return True
+
+
+def find_duplicate(nums: list[int]):
+    if not validate_nums(nums):
+        return False
+
+    unique_nums = set()
+    for num in nums:
+        if num in unique_nums:
+            return num
+        else:
+            unique_nums.add(num)
+
+    return False
